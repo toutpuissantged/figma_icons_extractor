daa440d620c30b665a70fe3031e688ce31fbab2a
toutpuissantged <amoussougedeon13@gmail.com>
2022-04-23 14:13:26 +0000

    mis en place du script svg

--- svg.py
+++ svg.py
@@ -6,6 +6,7 @@ import json
 import os
 from tokenize import String
 json
+import time
 
 class SVG_PATH_EXTRACTOR : 
   def __init__(self) -> None:
@@ -13,6 +14,7 @@ class SVG_PATH_EXTRACTOR :
     self.folder_path = ""
     self.svg_map = []
     self.current_dir = os.path.dirname(os.path.abspath(__file__))
+    self.folders_path = os.path.join(self.current_dir, "icons","linear")
     self.save_file = os.path.join(self.current_dir, "svg_path_map.json")
     pass
 
@@ -26,10 +28,11 @@ class SVG_PATH_EXTRACTOR :
       return ""
 
   def make_json_object( self, name , dir) -> None:
-    return '{"'+name+'": "'+dir.replace('\\','/')+'"}, \n'
+    return '{"name" : "'+name+'", "path" : "'+dir.replace('\\','/')+'"}, \n'
     
-  def list_svg_path_from_folder(self, folder_path) -> list:
+  def list_svg_path_from_folder(self ) -> list:
     svg_path_list = []
+    folder_path = self.folders_path
     svg_json = "["
     for file in os.listdir(folder_path):
       svg_dir = os.path.join(folder_path, file)
@@ -47,17 +50,40 @@ class SVG_PATH_EXTRACTOR :
         svg_path_list.append(os.path.join(folder_path, file))
     formated = svg_json+"]"
     self.save_output_to_file(self.save_file, formated)
-    print(formated)
+    #print(formated)
     return formated
 
   def save_output_to_file(self, file_path, content) -> None:
     with open(file_path, "w") as f:
       f.write(content)
+    pass  
+
+  def main(self) -> None:
+    self.list_svg_path_from_folder()
+
+class ExecutionTime :
+  def __init__(self) -> None:
+    self.start_time = 0
+    self.end_time = 0
+    self.execution_time = 0
     pass
 
+  def start(self) -> None:
+    self.start_time = time.time()
+    pass
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-folders_path = os.path.join(current_dir, "icons","linear")
+  def end(self) -> None:
+    self.end_time = time.time()
+    self.execution_time = self.end_time - self.start_time
+    pass
+
+  def get_execution_time(self) -> float:
+    return self.execution_time
+
+execution_time = ExecutionTime()
+execution_time.start()
 extractor = SVG_PATH_EXTRACTOR() 
-svg_path_list = extractor.list_svg_path_from_folder(folders_path)
+extractor.main()
+execution_time.end()
+print("Execution Time : ", execution_time.get_execution_time())
 
